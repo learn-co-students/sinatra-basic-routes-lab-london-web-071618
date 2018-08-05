f03aa613a8e241661529758ce374413080ebf38f
Ali Aziz <aliaziz@Alis-MacBook-Pro.local>
2018-08-05 16:57:49 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Ali."
+  end
+
+  get '/hometown' do
+    "My hometown is Acton."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Le Nozze Di Figaro."
+  end
 end
